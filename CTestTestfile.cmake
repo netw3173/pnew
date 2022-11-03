0eee7608c661b73e68d5b23eee3971ef732c67e0
netw3173 <113054391+netw3173@users.noreply.github.com>
2022-11-03 19:12:01 -0400

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/unbunt20/Desktop/pitcoinnewer/src
+# Build directory: /home/unbunt20/Desktop/pitcoinnewer/build/release/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
